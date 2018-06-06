786f98b111ee0653ba92309d5fbfd410672a2794
Ngoc Tu <ngocbeo196@gmail.com>
2018-06-06 11:35:40 -0500

    moved local understand to sentiment categorization and changed name to senv3.py

--- sentiment_categorization_execution/senv3.py
+++ sentiment_categorization_execution/senv3.py
@@ -1,152 +1,99 @@
 #!/usr/bin/env python
 
-# Extension of senv2.py
-# Added bigram collocation score
-
-import pandas as pd 
+import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import movie_reviews
+from nltk.collocations import BigramCollocationFinder, BigramAssocMeasures
+from nltk.tokenize import word_tokenize
+from nltk import bigrams 
+from nltk import FreqDist
 import sys
 import csv
-import math
-import random
-import nltk
 from nltk.corpus import stopwords
-import string
-from nltk.tokenize import word_tokenize
-
-bullfile = sys.argv[1]
-bearfile = sys.argv[2]
-reload(sys)
-sys.setdefaultencoding('utf-8')
+from os import path, listdir
 
 stop_words=set(stopwords.words('english'))
+nltk.download('punkt')
+reload(sys)
+sys.setdefaultencoding('utf-8')
+ 
+def word_feats(words):
+    return dict([(word, True) for word in words])
 
-# get rid of all punctuation
-# the get rid of words less than 3, stopwords and number
-# after initial screening is done (i.e: only words that do not start with $)
-def clean_word(word):
-	for ch in string.punctuation:
-		if ch in word:
-			word = word.replace(ch,'')
-	if len(word) >= 3 and not word in stop_words and not word.isdigit():
-		return word.lower()
-	else:
-		return None
+bigram_measure = BigramAssocMeasures()
+def best_bigram_word_feats(words,score_fn=bigram_measure.pmi,n=1000):
+	bigram_finder = BigramCollocationFinder.from_words(words)
+	bigram_finder.apply_word_filter(lambda w: not w.isalpha())
+	bigram_finder.apply_word_filter(lambda w: w in stop_words)
+	bigram_finder.apply_word_filter(lambda w: len(w)<2)
 
-# split the full twit into a list of words
-# clean the list to these criteria
-# # no '$' or 'http' in the beginning
-# then push these words  
-def cleanTwit(twit):
-	wordListRaw = twit.split()
-	wordList = []
-	for each in wordListRaw:
-		if each[0] != '$' and each[0:4] != 'http':
-			word = clean_word(each)
-			wordList.append(word) if not word is None else None
-	return wordList
+	bigrams = bigram_finder.nbest(score_fn,n)
+	d = dict([(bigram, True) for bigram in bigrams])
 
+	single_words = [word.lower() for index,word in enumerate(words) if len(word) > 3 and not word.isdigit() and not word in stop_words and words[index-1]!='$' and words[index-1]!='@']
+	d.update(word_feats(single_words))
+	return d
+
+# get all files from folder
+folder = sys.argv[1]
+files = listdir(folder)
+files = [f for f in files if f[-3:len(f)]=='csv']
 bulltwits = []
 beartwits = []
-with open(bullfile) as f:
-	bulltwits = f.readlines()
-with open(bearfile) as f:
-	beartwits = f.readlines()
 
-# get all words in tweets to calculate bigram measures
-# this is exclusively done to get bigrams so that we clean bigrams before doing 
-# any calculations
-# Return: a list of words
-def get_all_words_in_tweets(tweets):
-	all_words = []
-	for tweet in tweets:
-		all_words += word_tokenize(tweet)
-	return all_words
+# extract content of folder into two lists
+# one containing all bull twits
+# the other all bear twits
+for f in files:
+	with open(path.join(folder,f),'r') as csvfile:
+		reader = csv.reader(csvfile)
+		if f.find('bull') > -1:
+			for row in reader:
+				bulltwits.append(row[1])
+		elif f.find('bear') > -1:
+			for row in reader:
+				beartwits.append(row[1])
+		else:
+			continue
 
-all_bull_words = get_all_words_in_tweets(bulltwits)
-all_bear_words = get_all_words_in_tweets(beartwits)
-all_bullbear_words = all_bear_words + all_bull_words
+bull_words = []
+bear_words = []
 
-finder = nltk.BigramCollocationFinder.from_words(all_bullbear_words)
-bigram_measures = nltk.BigramAssocMeasures()
-finder.apply_word_filter(lambda w: not w.isalpha())
-finder.apply_word_filter(lambda w: w in stop_words)
-finder.apply_word_filter(lambda w: len(w)<3)
-score = sorted(finder.score_ngrams(bigram_measures.raw_freq), key=lambda x: x[1], reverse = True)
-for bi,s in score[:10]:
-	print(bi,s)
+# because there's more bearish news
+# limit the length of bullish list to its bearish list
+total_bear_tweets = len(beartwits)
+bulltwits = bulltwits[:total_bear_tweets]
+
+for line in bulltwits:
+	line = unicode(line,errors='ignore')
+	bull_words.append(word_tokenize(line.lower()))
+for line in beartwits:
+	line = unicode(line,errors='ignore')
+	bear_words.append(word_tokenize(line.lower()))
+
+# BigramCollocationFinder.from_words takes param of list of words
+bull_feats = [(best_bigram_word_feats(bull_word),'bull') for bull_word in bull_words]
+bear_feats = [(best_bigram_word_feats(bear_word),'bear') for bear_word in bear_words]
+
+accuracy_from_tests = []
+for i in range(10):
+	i = 0.1*i
+	bull_cutoff = int(len(bull_feats)*i)
+	bear_cutoff = int(len(bear_feats)*i)
+	bull_cutoff_up = int(len(bull_feats)*(i+0.1))
+	bear_cutoff_up = int(len(bear_feats)*(i+0.1))
 
 
+	trainfeats = bull_feats[0:bull_cutoff] + bull_feats[bull_cutoff_up:len(bull_feats)] + bear_feats[0:bear_cutoff] + bear_feats[bear_cutoff_up:len(bear_feats)]
+	testfeats = bull_feats[bull_cutoff:bull_cutoff_up] + bear_feats[bear_cutoff:bear_cutoff_up]
 
+	classifier = NaiveBayesClassifier.train(trainfeats)
+	classifier.show_most_informative_features(20)
 
-# bullbearlist: simple list with twits with no sentiment
-bullbearlist = [cleanTwit(x.strip()) for x in bulltwits] + [cleanTwit(x.strip()) for x in beartwits]
-bulltwits = [(cleanTwit(x.strip()),'Bullish') for x in bulltwits]
-beartwits = [(cleanTwit(x.strip()),'Bearish') for x in beartwits]
+	test_accuracy = nltk.classify.util.accuracy(classifier, testfeats)
+	accuracy_from_tests.append(test_accuracy)
+	print('Trained on '+str(len(trainfeats))+' rows')
+	print('Tested on '+str(len(testfeats))+' rows')
+	print('Test number %i accuracy: %f' % (i*10+1, test_accuracy))
 
-# bullbear: dict of twits and sentiment
-bullbear = bulltwits + beartwits
-random.shuffle(bullbear)
-bullbeartrain = bullbear[:int(len(bullbear)*0.5)]
-bullbeartest = bullbear[int(len(bullbear)*0.3):]
-
-# def get_words_in_tweets(tweets):
-#     all_words = []
-#     for (words, sentiment) in tweets:
-#     	all_words.extend(words)
-#     return all_words
-
-# def get_word_features(wordlist):
-#     wordlist = nltk.FreqDist(wordlist)
-#     word_features = dict((key,value) for key,value in wordlist.iteritems() if value>0)
-#     word_features = word_features.keys()
-#     return word_features
-
-# word_features = get_word_features(get_words_in_tweets(bullbeartrain))
-
-# def extract_features(document):
-#     document_words = set(document)
-#     features = {}
-#     for word in word_features:
-#         features['contains(%s)' % word] = (word in document_words)
-#     return features
-
-# # apply_features
-# # 1st param: name of function to be applied to each token; should return a feature set
-# # ie: a dict mapping feature names to feature values
-# # 2nd param: list of tokens to which function should be applied
-# training_set = nltk.classify.apply_features(extract_features, bullbeartrain)
-# print(type(training_set))
-# print("Length of training_set "+str(len(training_set)))
-
-# # apply classifier so that only features with a weight > 1.5 gets counted
-# classifier = nltk.NaiveBayesClassifier.train(training_set)
-# feature_set = classifier.most_informative_features(10)
-# print('Feature set type '+str(type(feature_set)))
-# for word,feature in feature_set:
-# 	print(word)
-# 	print(feature)
-
-# print(classifier.show_most_informative_features(10))
-
-# let model label data in test file
-# then cross-check labels with actual test
-# fr = pd.read_csv(ofile)
-# test_set = nltk.classify.apply_features(extract_features, bullbeartest)
-#print(test_set[0])
-#print(nltk.classify.accuracy(classifier,test_set))
-
-# testtwits = list(bullbeartest.keys())
-# correct = 0
-# wrong = 0
-# for i in range(len(testtwits)):
-# 	senTest = classifier.classify(extract_features(tweet.split()))
-# 	if sen == senTest:
-# 		correct += 1
-# 		csvwriter.writerow([tweet,sen,senTest,'Correct'])
-# 	else:
-# 		wrong += 1
-# 		csvwriter.writerow([tweet,sen,senTest,'Wrong'])
-# print('Correct ' + str(correct))
-# print('Wrong ' + str(wrong))
-
-# print(percentageCorrect)
+print('The average accuracy of 10 tests is '+str(sum(accuracy_from_tests)/len(accuracy_from_tests)))
