f904c373254a5677585f79b6f54be4581cbb817c
Ngoc Tu <ngocbeo196@gmail.com>
2018-06-18 11:51:33 -0500

    fixed data type to make if statements work

--- local_tests/count_sentiment.py
+++ local_tests/count_sentiment.py
@@ -15,9 +15,8 @@ with open('sentiment_counted','wb') as counted:
 		with open(path.join(folder,f),'r') as csvread:
 			reader = csv.reader(csvread)
 			for row in reader:
-				if row[1] > 0.5:
-					if row[2] == 'bull':
-						bull_count += 1
-					if row[2] == 'bear':
-						bear_count += 1
+				if row[2] == 'bull' and float(row[1]) > 0.5:
+					bull_count += 1
+				elif row[2] == 'bear' and float(row[1]) > 0.5:
+					bear_count += 1
 		writer.writerow([f,'bull',bull_count,'bear',bear_count])
