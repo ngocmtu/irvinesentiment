7d4cc1fed66aa088a517b66eb8787cafb3f7070e
Ngoc Tu <ngocbeo196@gmail.com>
2018-12-16 18:16:29 -0600

    added setup file

---
+++ sentiment_categorization_execution/setup_runfirst.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# TODO
+# ensure all modules are actually installed
+# not the current priority
+
+#import pip
+from os import path, listdir, makedirs
+
+# def import_or_install(package):
+#     try:
+#         __import__(package)
+#     except ImportError:
+#         pip.main(['install', package])
+
+if not path.exists('classified_data'):
+	makedirs('classified_data')
+if not path.exists('data_to_be_classified'):
+	makedirs('data_to_be_classified')
+if not path.exists('training_data'):
+	makedirs('training_data')
