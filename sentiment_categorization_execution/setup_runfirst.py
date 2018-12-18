bdeb6f9fff4d0666af5cf448aaf7f3fb2be74a0e
Ngoc Tu <ngocbeo196@gmail.com>
2018-12-17 20:42:53 -0600

    updated logic for this setup file

--- sentiment_categorization_execution/setup_runfirst.py
+++ sentiment_categorization_execution/setup_runfirst.py
@@ -4,18 +4,20 @@
 # ensure all modules are actually installed
 # not the current priority
 
-#import pip
-from os import path, listdir, makedirs
+import pip
+#from os import path, listdir, makedirs
 
-# def import_or_install(package):
-#     try:
-#         __import__(package)
-#     except ImportError:
-#         pip.main(['install', package])
+def import_or_install(package):
+    try:
+        __import__(package)
+    except ImportError:
+        pip.main(['install', package])
 
-if not path.exists('classified_data'):
-	makedirs('classified_data')
-if not path.exists('data_to_be_classified'):
-	makedirs('data_to_be_classified')
-if not path.exists('training_data'):
-	makedirs('training_data')
+import_or_install('nltk')
+
+# if not path.exists('classified_data'):
+# 	makedirs('classified_data')
+# if not path.exists('data_to_be_classified'):
+# 	makedirs('data_to_be_classified')
+# if not path.exists('training_data'):
+# 	makedirs('training_data')
